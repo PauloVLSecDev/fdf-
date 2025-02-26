0968657cd6e5241c6da3ae865bdccc13bf15d187
Paulo Vitor Lopes Meira <pvitor-l@student.42sp.org.br>
2025-02-26 18:47:31 -0300

    trying understand structs

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pvitor-l <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 17:07:24 by pvitor-l          #+#    #+#             */
-/*   Updated: 2025/02/26 16:13:33 by pvitor-l         ###   ########.fr       */
+/*   Updated: 2025/02/26 18:46:58 by pvitor-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,37 @@ int **parse_map(char *map, int width, int height)
 		i++;		
 	}
 }
-**/
-/*
-int **read_map(char *file)
+*/
+
+t_fdf init_fdf(void)
+{
+	t_fdf *fdf;
+	fdf = malloc(sizeof(s_fdf))
+	if (!fdf)
+		return (NULL)
+	fdf->zoom = 20;
+	fdf->shift_x = 20;
+	fdf->shift_y = 20;
+}
+
+t_fdf init_map(void)
+{
+	s_fdf *map;
+	map = malloc(sizeof(s_fdf))
+	map->angle_x = 0;
+	map->angle_y = 0;
+	map->angle_z = 0;
+}
+int **read_map(char *file) {
 {
 	int fd;
-	t_fdf *angle;
+	t_fdf *map;
 	char *line;
 
 	fd = open(file, O_RDONLY);
 	if (fd < 0)
 		return (NULL);
+	map = init_map();
 	while(line = get_next_line(fd) != NULL)
 	{
 		fdf.map = ft_split(line, ' ');
@@ -45,25 +65,31 @@ int **read_map(char *file)
 	}
 	return (fdf.map);
 }
-*/
-int 	main (int ac, char **av)
-{
-	t_fdf *fdf;
 
-	//read_map(av[1]);
-	(void)av;
-	fdf = malloc(sizeof(t_fdf));
+t_fdf init_winow(char *file)
+{
+	char *map_name;
 	fdf->mlx = mlx_init();
-	if (ac == 2)
+	map_name = file;
+	fdf->map = read_map(map_name)
+	if (!fdf->mlx)
 	{
-		if (!fdf->mlx)
-			return (1);
-		fdf->win = mlx_new_window(fdf->mlx, WIDTH, HEIGHT, "FDF");
-		if (!fdf->win)
-			return (1);
-		mlx_key_hook(fdf->win, close_window_ESC, NULL);
-		mlx_hook(fdf->win, 17, 0, close_window_X, NULL);
-		mlx_loop(fdf->mlx);
-		return (0);
+		free(fdf->mlx);
+		return (NULL);
 	}
+	fdf->win = mlx_new_window(fdf->mlx, WIDTH, HEIGHT, "FDF");
+	if (!fdf->win)
+		return (NULL);
+	mlx_key_hook(fdf->win, close_window_ESC, NULL);
+	mlx_hook(fdf->win, 17, 0, close_window_X, NULL);
+	mlx_loop(fdf->mlx);
+}
+int 	main (int argc, char **argv)
+{
+	check_args(argc, argv);
+	init_fdf();
+		//read_map(av[1]);
+		(void)av;
+		fdf = init_window(av[1]);
+	return (0);
 }
